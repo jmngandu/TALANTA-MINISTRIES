83ca35d73e24150342c6ebff8363790fdd3fcd36
James Ngandu <jamesmngandu@gmail.com>
2023-11-05 20:32:54 +0300

    added open and close button

--- Home/home.js
+++ Home/home.js
@@ -1,14 +1,16 @@
 const navSection = document.querySelector('.navs-btn');
 const closeBtn = document.querySelector('.closeBtn');
-const openBtn = document.querySelector('.closeBtn');
+const openBtn = document.querySelector('.openBtn');
 
-openBtn.addEventListener('click', show);
-closeBtn.addEventListener('click', close);
+openBtn.addEventListener('click', showMenu);
+closeBtn.addEventListener('click', closeMenu);
 
-function show() {
+function showMenu() {
 	navSection.style.display = 'flex';
-	navSection.style.top = '0';
+	openBtn.style.display = 'none';
+	openBtn.style.display = 'closeBtn';
 }
-function close() {
-	navSection.style.top = '-100%';
+function closeMenu() {
+	// navSection.style.top = '-100%';
+	closeBtn.style.display = 'block';
 }
